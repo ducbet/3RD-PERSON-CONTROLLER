7698b2d9de7bdff62102cb730feb00708cc2cc9f
ducbet <ducbet@users.noreply.github.com>
2022-08-21 15:16:40 +0900

    ep2 - Player Movement

--- Assets/InputManager/InputManager.cs
+++ Assets/InputManager/InputManager.cs
@@ -7,13 +7,22 @@ public class InputManager : MonoBehaviour
     public PlayerControls playerControls;
 
     public Vector2 movementInput;
+    public float verticalInput { get; private set; }
+    public float horizontalInput { get; private set; }
+
+
     private void OnEnable()
     {
         if (playerControls == null)
         {
             playerControls = new PlayerControls();
 
-            playerControls.PlayerMovement.Movement.performed += context => movementInput = context.ReadValue<Vector2>();
+            playerControls.PlayerMovement.Movement.performed += context =>
+            {
+                movementInput = context.ReadValue<Vector2>();
+                verticalInput = movementInput.y;
+                horizontalInput = movementInput.x;
+            };
         }
         playerControls.Enable();
     }
